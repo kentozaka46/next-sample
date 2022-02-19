3721d6e673816b70ecc49d988f93f69426a1dd55
kentozaka46 <kentoball52@gmail.com>
2022-02-19 15:23:09 +0900

    Script実装

--- my-app/pages/posts/first-post.tsx
+++ my-app/pages/posts/first-post.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 
 const FirstPost: NextPage = () => {
   return (
@@ -8,6 +9,13 @@ const FirstPost: NextPage = () => {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      ></Script>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
